5ba780acdc9dbd542ed6fb846150171cd64b6ac4
mimshins <mostafa.sh.coderino@gmail.com>
2021-09-12 20:02:40 +0430

    [:star:] add `useEventListener`

---
+++ src/useEventListener/useEventListener.ts
@@ -0,0 +1,114 @@
+import useGetLatest from "@utilityjs/use-get-latest";
+import * as React from "react";
+
+type ElementEventListener<K extends keyof HTMLElementEventMap> = (
+  this: HTMLElement,
+  ev: HTMLElementEventMap[K]
+) => void;
+
+type DocumentEventListener<K extends keyof DocumentEventMap> = (
+  this: Document,
+  ev: DocumentEventMap[K]
+) => void;
+
+type WindowEventListener<K extends keyof WindowEventMap> = (
+  this: Window,
+  ev: WindowEventMap[K]
+) => void;
+
+type Options = boolean | AddEventListenerOptions;
+
+type UseEventListener = {
+  <K extends keyof HTMLElementEventMap, T extends HTMLElement = HTMLElement>(
+    config: {
+      target: React.RefObject<T> | T | null;
+      eventType: K;
+      handler: ElementEventListener<K>;
+      options?: Options;
+    },
+    shouldAttach?: boolean
+  ): void;
+  <K extends keyof DocumentEventMap, T extends Document = Document>(
+    config: {
+      target: T | null;
+      eventType: K;
+      handler: DocumentEventListener<K>;
+      options?: Options;
+    },
+    shouldAttach?: boolean
+  ): void;
+  <K extends keyof WindowEventMap, T extends Window = Window>(
+    config: {
+      target: T | null;
+      eventType: K;
+      handler: WindowEventListener<K>;
+      options?: Options;
+    },
+    shouldAttach?: boolean
+  ): void;
+};
+
+const isOptionParamSupported = (): boolean => {
+  let optionSupported = false;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const fn = () => {};
+
+  try {
+    const opt = Object.defineProperty({}, "passive", {
+      get: () => {
+        optionSupported = true;
+        return null;
+      }
+    });
+
+    window.addEventListener("test", fn, opt);
+    window.removeEventListener("test", fn, opt);
+  } catch (e) {
+    return false;
+  }
+
+  return optionSupported;
+};
+
+const useEventListener: UseEventListener = (
+  config: {
+    target: HTMLElement | Window | Document | null;
+    eventType: string;
+    handler: unknown;
+    options?: Options;
+  },
+  shouldAttach = true
+): void => {
+  const { target = null, eventType, handler, options } = config;
+
+  const getHandler = useGetLatest(handler);
+  const getOptions = useGetLatest(options);
+
+  React.useEffect(() => {
+    const _handler_ = getHandler();
+    const _options_ = getOptions();
+
+    const listener = (event: Event) =>
+      (_handler_ as (ev: Event) => void)(event);
+
+    let thirdParam = _options_;
+
+    if (typeof _options_ !== "boolean") {
+      if (isOptionParamSupported()) thirdParam = _options_;
+      else thirdParam = undefined;
+    }
+
+    if (target != null && shouldAttach) {
+      target.addEventListener(eventType, listener, thirdParam);
+    }
+
+    return () => {
+      if (target != null) {
+        target.removeEventListener(eventType, listener, thirdParam);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [target, eventType, shouldAttach]);
+};
+
+export default useEventListener;
